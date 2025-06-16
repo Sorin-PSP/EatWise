8193b52d1ccaaa834441cdd645a0bce5f0f3d99f
Sorin-PSP <10sorin10@gmail.com>
2025-06-16 19:36:34 +0300

    EatWise16.06.25-19:30

--- src/registerSW.js
+++ src/registerSW.js
@@ -1,17 +1,7 @@
-import { registerSW as registerServiceWorker } from 'virtual:pwa-register'
-
 export const registerSW = () => {
   if ('serviceWorker' in navigator) {
-    // Register the service worker
-    const updateSW = registerServiceWorker({
-      onNeedRefresh() {
-        if (confirm('Este disponibilă o nouă versiune. Doriți să actualizați?')) {
-          updateSW(true)
-        }
-      },
-      onOfflineReady() {
-        console.log('Aplicația este pregătită pentru utilizare offline')
-      },
-    })
+    // Service worker registration is disabled for now
+    // Will be re-enabled when vite-plugin-pwa is properly configured
+    console.log('Service worker registration is currently disabled')
   }
 }
